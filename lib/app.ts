22a1255201df443088e7895b61cfd02b52531272
Frederick Dong <fredisawesome147@gmail.com>
2022-05-03 22:28:02 -0700

    remove actions stack

--- lib/app.ts
+++ lib/app.ts
@@ -2,7 +2,6 @@ import 'source-map-support/register'
 import { App, Environment } from 'aws-cdk-lib'
 // import CognitoStack from './cognito'
 import BackendStack from './backend'
-import ActionsStack from './actions'
 // import CloudwatchStack from './cloudwatch'
 import 'dotenv/config'
 
@@ -14,16 +13,11 @@ const stages = {
     // prod: 'us-west-1'
 }
 
-if (process.env.PR_NUM){ // check if called from AntAlmanac Github action
-    new ActionsStack(app, `github-actions-stack-${process.env.PR_NUM}`, {pr_num: process.env.PR_NUM, env: {region: 'us-east-1', account: account}})
-}
-else{
-    for (const [stage, region] of Object.entries(stages)) {
-        const env: Environment = { region: region, account: account }
-    
-        // new CognitoStack(app, `${stage}-${region}-Cognito`, { env, stage })
-        new BackendStack(app, `${stage}-${region}-Backend`, { env, stage })
-        // new CloudwatchStack(app, `${stage}-${region}-Cloudwatch`, { env, stage })
-    }
+for (const [stage, region] of Object.entries(stages)) {
+    const env: Environment = { region: region, account: account }
+
+    // new CognitoStack(app, `${stage}-${region}-Cognito`, { env, stage })
+    new BackendStack(app, `${stage}-${region}-Backend`, { env, stage })
+    // new CloudwatchStack(app, `${stage}-${region}-Cloudwatch`, { env, stage })
 }
     
